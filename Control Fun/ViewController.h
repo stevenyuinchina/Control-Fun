32ebb7996e26a253b2f6edf43b5583ee4745c92b
Yu Jin <stevenyuinchina@hotmail.com>
2015-08-29 23:25:43 +0800

    Test 1

--- Control Fun/ViewController.h
+++ Control Fun/ViewController.h
@@ -10,6 +10,11 @@
 
 @interface ViewController : UIViewController
 
+// 实现之前的两个功能，类似于VBA里面的call，就是调用了这两个功能
+
+- (IBAction)textFieldDoneEditing:(id)sender;
+
+-(IBAction)backgroundTap:(id)sender;
 
 @end
 
--- Control Fun/ViewController.m
+++ Control Fun/ViewController.m
@@ -10,10 +10,15 @@
 
 @interface ViewController ()
 
+// 这两个代码是在拖入textField之后，打开Assistant Editor，并把textField分别拖到这个地方自动生成的
+@property (weak, nonatomic) IBOutlet UITextField *nameField;
+@property (weak, nonatomic) IBOutlet UITextField *passField;
+
 @end
 
 @implementation ViewController
 
+// 以下两块都是自动生成的代码
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
@@ -22,6 +27,17 @@
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+// 添加“点击return之后返回输入界面"的功能
+- (IBAction)textFieldDoneEditing:(id)sender {
+    [sender resignFirstResponder];
+}
+
+// 添加"点击键盘区域以外的界面返回输入界面的功能"
+- (IBAction)backgroundTap:(id)sender {
+    [self.nameField resignFirstResponder];
+    [self.passField resignFirstResponder];
 }
 
 @end
